4a9813e322e76ac7da8c867e7c32c7e22d6b957e
Aaron Grisez <grise100@mail.chapman.edu>
2019-08-25 11:38:17 -0700

    Added email normalization

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,3 +15,13 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+    
+    def test_new_user_email_normalized(self):
+        """Test that the domain name for an email is not case sensitive"""
+        email = 'test@TEST.COM'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='test123'
+        )
+        self.assertEqual(user.email, email.lower())
+    
